2a4aa6dd1845afe64b19b0a08ec8186bcb683518
lumierevy <lumieremahoutei01@gmail.com>
2024-06-19 10:52:42 +0700

    edit design text hero

--- src/app/page.js
+++ src/app/page.js
@@ -13,10 +13,18 @@ export default async function Home() {
       <Header />
 
       <div className="p-12 space-y-4 sm:space-y-0 sm:flex justify-between">
-        <h1 className="text-lg sm:text-2xl md:text-4xl">
-          Vendor Contact for Indonesia Film Festival 2024
+        <h1 className="text-xl sm:text-2xl md:text-4xl tracking-wide">
+          Vendor Contact for{" "}
+          <span className="font-bold text-5xl sm:text-2xl md:text-4xl bg-clip-text text-transparent bg-gradient-to-r from-cyan-500  via-blue-600-600 to-emerald-500">
+            Indonesia Film Festival{" "}
+            <span className="font-bold text-6xl sm:text-2xl md:text-4xl bg-clip-text text-transparent bg-gradient-to-r from-emerald-300  via-emerald-500 to-cyan-500">
+              2024
+            </span>
+          </span>
         </h1>
-        <AddVendorForm />
+        <div className="flex justify-end">
+          <AddVendorForm />
+        </div>
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-12">
         {data.map((item) => {
